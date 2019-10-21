b61cc2b88289332b7d4e1e2fdd2e3df8f9ca2cae
Thiago Cardoso <intelxd@gmail.com>
2019-10-21 20:12:11 -0300

    Fix default statement for fields

--- db/migrate/20191021152240_create_parks.rb
+++ db/migrate/20191021152240_create_parks.rb
@@ -2,8 +2,8 @@ class CreateParks < ActiveRecord::Migration[5.2]
   def change
     create_table :parks do |t|
       t.string :plate
-      t.boolean :left, null: false, default: false
-      t.boolean :paid, null: false, default: false
+      t.boolean :left, null: false, :default => false
+      t.boolean :paid, null: false, :default => false
       t.datetime :left_in
 
       t.timestamps
